feb2a90da0d91d7e5936e7063807546a615df320
venkeyy <60574581+venkeyy@users.noreply.github.com>
2021-05-16 19:54:15 +0530

    Update Find_the_Runner-Up_Score!.py

--- Find_the_Runner-Up_Score!.py
+++ Find_the_Runner-Up_Score!.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
     
-    a = max(arr)  # Prints the max elements in the list 
+    a = max(arr)  # Prints the max elements in the list , arr = array
     c = arr.count(a)  # Count the list 
     for i in range(c):
         arr.remove(a)  #  Remove the max elemnet in the list 
